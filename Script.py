5cab5abf966e190cc950703e50b6ee546b86ad35
rajatdeveloper <singhal.rajat97@gmail.com>
2018-07-09 10:59:29 +0530

    Database Created

--- Script.py
+++ Script.py
@@ -1,14 +1,22 @@
-'''import bluetooth
-
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
-print(nearby_devices)
-for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
-'''
-
+import bluetooth
 import os, random
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
+import sqlite3
+
+def findbluetooth():
+	print("SEARCHING.....")
+	try:
+		nearby_devices = bluetooth.discover_devices(lookup_names=True)
+		for i in range (len(nearby_devices)):
+			print("  %s - %s" % (i+1,nearby_devices[i][1]))
+	except:
+		print("no device found")
+	j=int(input("Select Your Bluetooth Device Or press zero to Search again..."))
+	if(j==0):
+		return findbluetooth()
+	else:
+		return nearby_devices[j-1][0]
 
 def encrypt(key, filename):
 	chunksize = 64*1024
@@ -62,19 +70,45 @@ def getKey(password):
 	hasher = SHA256.new(password)
 	return hasher.digest()
 
-def Main():
-	choice = raw_input("Would you like to (E)ncrypt or (D)ecrypt?: ")
 
+def Main():
+	con = sqlite3.connect('Credentials.db')
+	con.text_factory = str
+	cursor = con.cursor()
+	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+	try:
+		cursor.execute('''CREATE TABLE CREDENTIALS
+		(ID INTEGER PRIMARY KEY    AUTOINCREMENT,
+		FILENAME CHAR    NOT NULL,
+		KEY      CHAR    NOT NULL);''')
+	except:
+		pass
+	choice = raw_input("Would you like to (E)ncrypt or (D)ecrypt?: ")
 	if choice == 'E':
 		filename = raw_input("File to encrypt: ")
-		password = raw_input("Password: ")
-		encrypt(getKey(password), filename)
+		device_mac = findbluetooth()
+	 	password = raw_input("Password: ")+device_mac
+		password = getKey(password)
+		cursor.execute("INSERT INTO CREDENTIALS (FILENAME,KEY) \
+		VALUES ( ?,? )",(filename,password,));
+		con.commit()
+		con.close()
+		encrypt(password, filename)
+		os.popen('attrib +h (encrypted)' + filename)
 		os.remove(filename)
 	elif choice == 'D':
-		filename = "(encrypted)"+raw_input("File to decrypt: ")
-		password = raw_input("Password: ")
-		decrypt(getKey(password), filename)
-		os.remove(filename)
+	  	device_mac = findbluetooth()
+		password = raw_input("Password: ")+device_mac
+		password = getKey(password)
+		files = cursor.execute("SELECT FILENAME FROM CREDENTIALS WHERE KEY = ?",(password,));
+		files = list(files)
+		for i in range(len(files)):
+			print(str(i+1)+"-"+str(files[i][0]))
+		fileindex = int(input())
+		filename = "(encrypted)"+files[fileindex-1][0]
+		con.close()
+		decrypt(password, filename)
+	 	os.remove(filename)
 	else:
 		print "No Option selected, closing..."
 
